03d3a3e532e8b6bab188a262c69c172f7c971661
Albert Hopkins <marduk@python.net>
2014-01-07 10:03:31 -0500

    tp_library: Add test for adding utf-8-encoded filenames to the queue.

--- tp_library/tests.py
+++ tp_library/tests.py
@@ -3,7 +3,7 @@ import os
 
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
-from django.test import SimpleTestCase
+from django.test import TestCase
 
 from teamplayer.lib import users
 from teamplayer.models import Entry
@@ -13,9 +13,11 @@ from mock import patch
 __dir__ = os.path.dirname(__file__)
 SILENCE = os.path.join(__dir__, '..', 'teamplayer', 'tests', 'data',
                        'silence.mp3')
+DATURA = os.path.join(__dir__, '..', 'teamplayer', 'tests', 'data',
+                      'd\u0101tura.mp3')  # dātura.mp3
 
 
-class SongFileTest(SimpleTestCase):
+class SongFileTest(TestCase):
     def test_not_exists(self):
         """Demonstrate the exists() for nonexistant files method."""
         # given the song pointing to a non-existant file
@@ -49,7 +51,7 @@ class SongFileTest(SimpleTestCase):
         self.assertTrue(exists)
 
 
-class AddToQueueTest(SimpleTestCase):
+class AddToQueueTest(TestCase):
     def setUp(self):
         self.song = SongFile.objects.create(
             filename=SILENCE,
@@ -72,6 +74,44 @@ class AddToQueueTest(SimpleTestCase):
         """add_to_queue view"""
         self.client.login(username='test', password='test')
         response = self.client.post(self.url, {'song_id': self.song.pk})
+        self.assertEqual(response.status_code, 200)
+        data = json.loads(response.content.decode('utf-8'))
+        self.assertTrue('error' not in data)
+
+        # look in the user's queue, we should have that song
+        song = Entry.objects.filter(queue=self.player.queue)
+        self.assertEqual(song.count(), 1)
+
+
+class AddSongWithUTF8Filename(TestCase):
+    # The reason why I wrote this test was: It was failing in deployment.  When
+    # I tried to add a song with filename Dātura, it gave the error:
+    # "UnicodeEncodeError: 'ascii' codec can't encode character '\u0101' in
+    # position 63: ordinal not in range(128)".  I could not duplicate the error
+    # in this test.  When I added a print(type(songfile.name)) in the deployed
+    # version it suddenly started to work... wtf.  If it happens again I'll
+    # have to come back to this and revise the test.
+    def setUp(self):
+        self.datura = SongFile.objects.create(
+            filename=DATURA,
+            artist='Tori Amos',
+            title='D\u0101tura',
+            album='To Venus and Back',
+            genre='Unknown',
+            length=300,
+            filesize=3000,
+            station_id=1,
+            mimetype='audio/mp3',
+            added_by=User.objects.get(username='DJ Ango'),
+        )
+        self.url = reverse('tp_library.views.add_to_queue')
+        self.user = users.create_user(username='test', password='test')
+        self.player = self.user.player
+
+    @patch('tp_library.views.IPCHandler.send_message')
+    def test_add_utf8_filename(self, mock):
+        self.client.login(username='test', password='test')
+        response = self.client.post(self.url, {'song_id': self.datura.pk})
         self.assertEqual(response.status_code, 200)
         data = json.loads(response.content.decode('utf-8'))
         self.assertTrue('error' not in data)
