215d6bf11aeb4cc7d036a63bac556385f708a385
Albert Hopkins <marduk@python.net>
2015-03-17 14:25:08 -0400

    middleware: create a Player on an authenticated user if none exists
    This can be used for cases where the User already exists on the system but
    there is no Player object, such as when alternative authentication and/or
    registration  is used ;-)

--- teamplayer/middleware.py
+++ teamplayer/middleware.py
@@ -1,8 +1,18 @@
+from .models import Player, Queue
+
 
 class TeamPlayerMiddleware(object):
     """Special middleware for TeamPlayer"""
     def process_request(self, request):
         """Add a "player" attribute to the request object."""
-        if hasattr(request, 'user') and hasattr(request.user, 'player'):
-            request.player = request.user.player
+        if hasattr(request, 'user') and request.user.is_authenticated():
+            user = request.user
+            try:
+                player = Player.objects.get(user=user)
+            except Player.DoesNotExist:
+                queue = Queue.objects.create()
+                player = Player.objects.create(
+                    user=user, queue=queue, dj_name='')
+            request.player = player
+
         return None
--- teamplayer/tests/test_middleware.py
+++ teamplayer/tests/test_middleware.py
@@ -1,8 +1,10 @@
-from teamplayer.middleware import TeamPlayerMiddleware
-from teamplayer.models import Player
-
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
 from django.test import TestCase
 from django.test.client import RequestFactory
+
+from teamplayer.middleware import TeamPlayerMiddleware
+from teamplayer.models import Player
 
 
 class TeamPlayerMiddlewareTestCase(TestCase):
@@ -21,3 +23,16 @@ class TeamPlayerMiddlewareTestCase(TestCase):
 
         self.assertEqual(result, None)
         self.assertEqual(request.player, player)
+
+    def test_user_with_no_player(self):
+        # given the User with no Player
+        user = User.objects.create_user(username='test', password='***')
+
+        # when we login and go to a page
+        url = reverse('teamplayer.views.home')
+        self.client.login(username='test', password='***')
+        self.client.get(url)
+
+        # then the middleware gives the user a player object
+        player = Player.objects.filter(user=user)
+        self.assertEqual(player.count(), 1)
