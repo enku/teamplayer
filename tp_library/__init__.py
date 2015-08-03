32579967fb379362a23caaaf7b4c3e34fea03f08
Albert Hopkins <marduk@python.net>
2015-08-03 15:43:38 -0400

    tp_library/search: highlight search terms
    Using django-haystack's "highlight" template tag, which adds a css class
    to the search terms, and a little css to add subtle highlighting.
    
    A custom highlighter is included (tp_library.Highlighter) because the
    default haystack highlighter truncates the text (why?!) and that behavior
    is undesirable.

--- tp_library/__init__.py
+++ tp_library/__init__.py
@@ -1,4 +1,16 @@
 from django.apps import AppConfig
+from django.utils.html import strip_tags
+from haystack.utils import Highlighter as BaseHighlighter
+
+
+class Highlighter(BaseHighlighter):
+    """A haystack highlighter that does not truncate"""
+    def highlight(self, text_block):
+        self.text_block = strip_tags(text_block)
+        text_len = len(self.text_block)
+        highlight_locations = self.find_highlightable_words()
+
+        return self.render_html(highlight_locations, 0, text_len)
 
 
 class TPLibraryConfig(AppConfig):
