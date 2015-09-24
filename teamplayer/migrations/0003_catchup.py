7c3721b44ee2831b9c4cbe0d50da802a39468cbf
Albert Hopkins <marduk@python.net>
2015-09-24 10:21:13 -0400

    Catch up on migrations.
    I had made a couple schema changes and neglected to add migrations
    for them.  Added.

---
+++ teamplayer/migrations/0003_catchup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('teamplayer', '0002_initial_data'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='mood',
+            name='timestamp',
+            field=models.DateTimeField(auto_now_add=True),
+        ),
+        migrations.AlterField(
+            model_name='station',
+            name='creator',
+            field=models.OneToOneField(to='teamplayer.Player'),
+        ),
+    ]
