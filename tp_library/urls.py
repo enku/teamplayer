b71d92e490f4bc910bb1e7c5e9e494290e70e8bb
Albert Hopkins <marduk@python.net>
2016-11-17 21:49:44 -0500

    tp_library.urls: update to latest supporte urlconf format
    I.e. don't use patterns()

--- tp_library/urls.py
+++ tp_library/urls.py
@@ -1,9 +1,10 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
-urlpatterns = patterns(
-    '',
-    url(r'^search/', 'tp_library.views.song_search', name='tp_library_search'),
-    url(r'^add_to_queue', 'tp_library.views.add_to_queue'),
-    url(r'^song/(\d+)$', 'tp_library.views.get_song'),
-    url(r'^feeds/(\d+)/rss$', 'tp_library.views.feed'),
-)
+from tp_library import views
+
+urlpatterns = [
+    url(r'^search/', views.song_search, name='tp_library_search'),
+    url(r'^add_to_queue', views.add_to_queue),
+    url(r'^song/(\d+)$', views.get_song),
+    url(r'^feeds/(\d+)/rss$', views.feed),
+]
