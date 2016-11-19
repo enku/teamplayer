c36d03bd410ee46e9f44ae7acbe5137ce00f69ff
Albert Hopkins <marduk@python.net>
2016-11-19 07:16:33 -0500

    docker: entrypoint.sh: wait a bit before calling migrate
    Sometimes, on my machine, the database container isn't up and listening before
    the wsgi container tries to connect to it.  This usually happens the first
    time the containers are started.  This change puts a small sleep before the
    wsgi container calls migrate in hopes that the database is ready by then.

--- tools/docker/teamplayer/entrypoint.sh
+++ tools/docker/teamplayer/entrypoint.sh
@@ -6,6 +6,7 @@ DJANGO_SETTINGS_MODULE=project.settings ; export DJANGO_SETTINGS_MODULE
 MANAGE_PY=/opt/teamplayer/manage.py
 
 # migrate the data
+sleep 3
 python3 $MANAGE_PY migrate
 python3 $MANAGE_PY spindoctor &
 gunicorn -w 4 --bind 0.0.0.0:9000 project.wsgi
