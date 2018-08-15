19dfb95d6988b7cbab8fc79e26a5fe7dcdb6332a
Albert Hopkins <marduk@letterboxes.org>
2018-08-14 22:49:39 -0700

    urls.py: switch to classed-bass login view
    The function-based view is removed in Django 2.1

--- tools/urls.py
+++ tools/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import include, url
 
-from django.contrib.auth.views import login as django_login
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
-    url(r'^accounts/login/$', django_login),
+    url(r'^accounts/login/$', LoginView.as_view()),
     url(r'', include('teamplayer.urls')),
 ]
