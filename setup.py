c4b8f1483f67c663b65092b320ac890389749677
Albert Hopkins <marduk@letterboxes.org>
2018-08-14 22:50:55 -0700

    support Django 2.0 & 2.1
    This change removes support for Django 1.11 and adds support for Django
    2.1.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'Django>=1.11,<2.1',
+        'Django>=2.0,<2.2',
         'django-haystack>=2.8,<2.9',
         'djangorestframework>=3.7,<3.8',
         'mutagen>=1.29,<1.30',
