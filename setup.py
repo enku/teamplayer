bcb54b7c2c85b52032cf56c1117d8cd8035789bd
Albert Hopkins <marduk@letterboxes.org>
2020-08-08 10:41:44 -0700

    setup.py: auto-format
    Replaced single quotes with double quotes because that's what my editor
    does because that's what black does.

--- setup.py
+++ setup.py
@@ -3,27 +3,27 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='TeamPlayer',
-    version=__import__('teamplayer').version_string(show_revision=False),
-    description='A Democratic Internet Radio Station',
-    author='Albert Hopkins',
-    author_email='marduk@letterboxes.org',
+    name="TeamPlayer",
+    version=__import__("teamplayer").version_string(show_revision=False),
+    description="A Democratic Internet Radio Station",
+    author="Albert Hopkins",
+    author_email="marduk@letterboxes.org",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'Django>=2.2.8,<2.3',
-        'django-haystack>=2.8,<2.9',
-        'djangorestframework>=3.7,<3.8',
-        'mutagen>=1.29,<1.30',
-        'pylast>=1.6,<1.7',
-        'python-mpd2',
-        'tornado',
+        "Django>=2.2.8,<2.3",
+        "django-haystack>=2.8,<2.9",
+        "djangorestframework>=3.7,<3.8",
+        "mutagen>=1.29,<1.30",
+        "pylast>=1.6,<1.7",
+        "python-mpd2",
+        "tornado",
     ],
     entry_points={
-        'teamplayer.autofill_strategy': [
-            'contiguous = teamplayer.lib.autofill:auto_fill_contiguous',
-            'mood = teamplayer.lib.autofill:auto_fill_mood',
-            'random = teamplayer.lib.autofill:auto_fill_random',
+        "teamplayer.autofill_strategy": [
+            "contiguous = teamplayer.lib.autofill:auto_fill_contiguous",
+            "mood = teamplayer.lib.autofill:auto_fill_mood",
+            "random = teamplayer.lib.autofill:auto_fill_random",
         ],
     },
 )
