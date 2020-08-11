bef9282a1b3bebde1817f43ac3642ec68b4f3c2c
Albert Hopkins <marduk@letterboxes.org>
2020-08-10 19:56:10 -0700

    docker: entrypoing.sh add ability to walk a directory on startup
    If the environment variable `TEAMPLAYER_WALK` is defined in the
    container, then it will run `python3 manage.py tplibrarywalk $TEAMPLAYER_WALK`
    This can be used in docker-compose. For example if you create a
    `docker-compose.override.yml` with the following:
    
    ```
    version: '3.8'
    services:
      app:
        environment:
          - TEAMPLAYER_WALK=/usr/share/music
        volumes:
          - ~/Music:/usr/share/music
    ```

--- tools/docker/entrypoint.sh
+++ tools/docker/entrypoint.sh
@@ -17,6 +17,10 @@ sleep 3
 python3 $MANAGE_PY migrate
 python3 $MANAGE_PY spindoctor &
 
+if [ -n "${TEAMPLAYER_WALK}" ] ; then
+    python3 $MANAGE_PY tplibrarywalk "${TEAMPLAYER_WALK}" &
+fi
+
 if [ "${DJANGO_DEBUG}" -ne 0 ] ; then
     python3 $MANAGE_PY runserver -v2 0.0.0.0:9000
 else
