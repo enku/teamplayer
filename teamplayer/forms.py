285302c2a37614a7c841b294c6e77d408ae4cf65
Albert Hopkins <marduk@letterboxes.org>
2025-01-25 21:32:14 -0600

    forms: use set (literal) for allowable characters

--- teamplayer/forms.py
+++ teamplayer/forms.py
@@ -5,6 +5,13 @@ from django import forms
 
 from teamplayer import models
 
+ALLOWABLE_DJ_NAME_CHARACTERS = {
+    *string.ascii_letters,
+    *string.digits,
+    *string.punctuation,
+    *" _-",
+}
+
 
 class EditStationForm(forms.Form):
     name = forms.CharField(max_length=128)
@@ -48,9 +55,6 @@ class CreateStationForm(forms.Form):
 
 class ChangeDJNameForm(forms.Form):
     dj_name = forms.CharField(max_length=25, required=False)
-    allowable_characters = (
-        string.ascii_letters + string.digits + string.punctuation + " _-"
-    )
 
     def __init__(self, *args: Any, **kwargs: Any) -> None:
         self.player = kwargs.pop("player")
@@ -79,8 +83,7 @@ class ChangeDJNameForm(forms.Form):
         if name.lower() in ("dj ango", "dj_ango", "django"):
             return False
 
-        for c in name:
-            if c not in self.allowable_characters:
-                return False
+        if not set(name) <= ALLOWABLE_DJ_NAME_CHARACTERS:
+            return False
 
         return True
